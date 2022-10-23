de1ec9054a37d204dfcbe179b191be1952a575db
Pedro Melo <pedrogmelo@Pedro-Melo.Home>
2022-10-23 20:37:15 +0100

    first commit

---
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.quizapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStart: Button = findViewById(R.id.btnStart)
+        val etName: EditText = findViewById(R.id.etName)
+        btnStart.setOnClickListener {
+
+            if (etName.text.isEmpty()) {
+                Toast.makeText(this, "Enter your name", Toast.LENGTH_LONG).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+}
